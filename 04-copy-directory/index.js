4e550b5bf74621f2f210be38dbe3fa0dc9e718bc
Andrey Kleshenko <andreykleshenko@gmail.com>
2022-11-08 00:17:47 +0300

    feat: add solution for task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,51 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (error) => {
+  if (error) throw error;
+});
+
+function copyDir() {
+  fs.readdir(
+    path.join(__dirname, 'files-copy'),
+    { withFileTypes: true },
+    (error, files) => {
+      if (error) {
+        throw error;
+      } else {
+        files.forEach((element) => {
+          fs.unlink(
+            path.join(path.join(__dirname, 'files-copy'), element.name),
+            function (error) {
+              if (error) console.log(error.message);
+            }
+          );
+        });
+      }
+    }
+  );
+
+  fs.readdir(
+    path.join(__dirname, 'files'),
+    { withFileTypes: true },
+    (error, files) => {
+      if (error) {
+        throw error;
+      } else {
+        files.forEach((element) => {
+          if (element.isFile()) {
+            fs.copyFile(
+              path.join(path.join(__dirname, 'files'), element.name),
+              path.join(path.join(__dirname, 'files-copy'), element.name),
+              function (error) {
+                if (error) console.log(error.message);
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+}
+
+copyDir();
